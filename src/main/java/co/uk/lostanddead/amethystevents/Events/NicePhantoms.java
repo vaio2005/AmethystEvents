cf2d2d03d9ca46a6a9d20e6e2da70cde1bcfccd0
Poulette <63922760+PouletteMC@users.noreply.github.com>
2021-04-27 22:03:52 +0200

    should be done ?

--- src/main/java/co/uk/lostanddead/amethystevents/Events/NicePhantoms.java
+++ src/main/java/co/uk/lostanddead/amethystevents/Events/NicePhantoms.java
@@ -3,11 +3,15 @@ package co.uk.lostanddead.amethystevents.Events;
 //Grab ya imports
 
 import co.uk.lostanddead.amethystevents.AmethystEvents;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Phantom;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitScheduler;
 
 
 public class NicePhantoms implements Listener {
@@ -22,14 +26,24 @@ public class NicePhantoms implements Listener {
         return ChatColor.PURPLE + "Nice Phantoms";
     }
     
-    public String getDescription() {
-        return "Phantoms are nice now"
-    }
+    public String getDescription() { return "Phantoms are nice now"; }
 
     @EventHandler
-    public void EntityDamageByEntityEvent(EntityDamageByEntityEvent event){
-        if(event.getDamager() instanceof Phantom) {
+    public void EntityDamageByEntityEvent(EntityDamageByEntityEvent event) {
+
+        if (event.getDamager() instanceof Phantom && event.getEntity() instanceof Player) {
+            BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
+
             event.setDamage(0);
+            Player p = (Player) event.getEntity();
+            p.setAbsorptionAmount(2);
+
+            scheduler.scheduleSyncDelayedTask((Plugin) this, new Runnable() {
+                @Override
+                public void run() {
+                    p.setAbsorptionAmount(0);
+                }
+            }, 100);
         }
     }    
 }
